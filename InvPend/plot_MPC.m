c4c71ea4226fbc2ea5d07caa9f1a68ebeb85585e
Lucien Morey <lucienp@live.com.au>
2023-05-03 09:58:27 +1000

    basic plot for mpc coppied from lqr

--- InvPend/plot_MPC.m
+++ InvPend/plot_MPC.m
@@ -14,4 +14,27 @@ theta_hat=x_hat(:,3)*180/pi;
 xc_ref=x_ref(:,1);
 
 figure(2021)
-%Create your plot here
+subplot(311)
+plot(time,u)
+ylabel('$u~(Nm)$','fontsize',16,'interpreter','latex')
+hold on
+plot([0 60], [-15 -15],'--k')
+plot([0 60], [15 15],'--k')
+yticks([-25 -10 0 10 25])
+hold off
+grid
+axis([0 60 -30 30])
+title('MPC','fontsize',16,'interpreter','latex')
+
+subplot(312)
+plot(time,xc,time,xc_hat,time,xc_ref,'--k')
+legend('$x_c$','$\hat{x}_c$','$x_c^{\star}$','fontsize',16,'interpreter','latex')
+ylabel('$x_c~ (m)$','fontsize',16,'interpreter','latex')
+grid
+subplot(313)
+plot(time,theta,time,theta_hat)
+ylabel('$\theta~ (deg)$','fontsize',16,'interpreter','latex')
+xlabel('Time (s)','fontsize',16,'interpreter','latex')
+legend('$\theta(t)$','$\hat{\theta}(t)$','fontsize',16,'interpreter','latex')
+grid
+
