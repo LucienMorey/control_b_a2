2aeaa470530907508ca5610aac9cc950257a7692
Lucien Morey <lucienp@live.com.au>
2023-05-03 23:03:09 +1000

    update information on plots for lqr simulations

--- InvPend/plot_LQR.m
+++ InvPend/plot_LQR.m
@@ -22,19 +22,59 @@ hold on
 plot([0 60], [-25 -25],'--k')
 plot([0 60], [25 25],'--k')
 yticks([-25 -10 0 10 25])
+xticks(0:5:60);
 hold off
 grid
 axis([0 60 -30 30])
 title('LQR','fontsize',16,'interpreter','latex')
 
 subplot(312)
-plot(time,xc,time,xc_hat,time,xc_ref,'--k')
-legend('$x_c$','$\hat{x}_c$','$x_c^{\star}$','fontsize',16,'interpreter','latex')
+plot(time,xc,time,xc_hat,time,xc_ref,'--k', time,xc_ref*0.9,':k', time,xc_ref*1.1,':k')
+legend('$x_c$','$\hat{x}_c$','$x_c^{\star}$', '$x_c^{\star}\pm10\%$', 'fontsize',16,'interpreter','latex')
 ylabel('$x_c~ (m)$','fontsize',16,'interpreter','latex')
 grid
+xticks(0:5:60);
 subplot(313)
 plot(time,theta,time,theta_hat)
 ylabel('$\theta~ (deg)$','fontsize',16,'interpreter','latex')
 xlabel('Time (s)','fontsize',16,'interpreter','latex')
 legend('$\theta(t)$','$\hat{\theta}(t)$','fontsize',16,'interpreter','latex')
+grid
+xticks(0:5:60);
+
+
+figure(2022)
+
+
+subplot(211)
+plot(time,xc, time,xc_hat)
+ylabel('$x_c(m)$','fontsize',16,'interpreter','latex')
+legend('$x_c(t)$','$\hat{x_c}(t)$','fontsize',16,'interpreter','latex')
+axis([0 10 -2 2])
+grid
+title('Filter Convergence','fontsize',16,'interpreter','latex')
+
+subplot(212)
+plot(time,theta, time,theta_hat)
+ylabel('$\theta(deg)$','fontsize',16,'interpreter','latex')
+legend('$\theta(t)$','$\hat{\theta}(t)$','fontsize',16,'interpreter','latex')
+axis([0 10 -8 8])
+grid
+
+figure(2022)
+
+
+subplot(211)
+plot(time,xc, time,xc_hat)
+ylabel('$x_c(m)$','fontsize',16,'interpreter','latex')
+legend('$x_c(t)$','$\hat{x_c}(t)$','fontsize',16,'interpreter','latex')
+axis([0 10 -2 2])
+grid
+title('Filter Convergence','fontsize',16,'interpreter','latex')
+
+subplot(212)
+plot(time,theta, time,theta_hat)
+ylabel('$\theta(deg)$','fontsize',16,'interpreter','latex')
+legend('$\theta(t)$','$\hat{\theta}(t)$','fontsize',16,'interpreter','latex')
+axis([0 10 -8 8])
 grid
