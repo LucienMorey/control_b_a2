360615e26dd7c554838383c53ed0c66559528c56
Lucien Morey <lucienp@live.com.au>
2023-05-04 16:37:17 +1000

    add extra plots in case lqr needs to be redone

--- InvPend/plot_LQR.m
+++ InvPend/plot_LQR.m
@@ -42,6 +42,34 @@ legend('$\theta(t)$','$\hat{\theta}(t)$','fontsize',16,'interpreter','latex')
 grid
 xticks(0:5:60);
 
+figure(2024)
+subplot(311)
+plot(time,u)
+ylabel('$u~(Nm)$','fontsize',16,'interpreter','latex')
+hold on
+plot([0 60], [-15 -15],'--k')
+plot([0 60], [15 15],'--k')
+yticks([-25 -10 0 10 25])
+xticks(0:5:60);
+hold off
+grid
+axis([0 60 -30 30])
+title('LQR','fontsize',16,'interpreter','latex')
+
+subplot(312)
+plot(time,xc,time,xc_hat,time,xc_ref,'--k', time,xc_ref*0.9,':k', time,xc_ref*1.1,':k')
+legend('$x_c$','$\hat{x}_c$','$x_c^{\star}$', '$x_c^{\star}\pm10\%$', 'fontsize',16,'interpreter','latex')
+ylabel('$x_c~ (m)$','fontsize',16,'interpreter','latex')
+grid
+xticks(0:5:60);
+subplot(313)
+plot(time,theta,time,theta_hat)
+ylabel('$\theta~ (deg)$','fontsize',16,'interpreter','latex')
+xlabel('Time (s)','fontsize',16,'interpreter','latex')
+legend('$\theta(t)$','$\hat{\theta}(t)$','fontsize',16,'interpreter','latex')
+grid
+xticks(0:5:60);
+
 
 figure(2022)
 
@@ -61,14 +89,14 @@ legend('$\theta(t)$','$\hat{\theta}(t)$','fontsize',16,'interpreter','latex')
 axis([0 10 -8 8])
 grid
 
-figure(2022)
+figure(2026)
 
 
 subplot(211)
 plot(time,xc, time,xc_hat)
 ylabel('$x_c(m)$','fontsize',16,'interpreter','latex')
 legend('$x_c(t)$','$\hat{x_c}(t)$','fontsize',16,'interpreter','latex')
-axis([0 10 -2 2])
+axis([10 20 -1 3])
 grid
 title('Filter Convergence','fontsize',16,'interpreter','latex')
 
@@ -76,5 +104,5 @@ subplot(212)
 plot(time,theta, time,theta_hat)
 ylabel('$\theta(deg)$','fontsize',16,'interpreter','latex')
 legend('$\theta(t)$','$\hat{\theta}(t)$','fontsize',16,'interpreter','latex')
-axis([0 10 -8 8])
+axis([10 20 0 700])
 grid
